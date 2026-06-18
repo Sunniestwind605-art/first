0b87174400cb43a952ac725108b7113b585cd055
Joseph <josemokoena7@gmail.com>
2026-06-19 00:29:38 +0200

    A change made in a new branch, koloi

--- gittest1.cpp
+++ gittest1.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main() {
-  for (int i=10 ; i>=0 ; i--){
+  /* (int i=10 ; i>=0 ; i--){
     cout<< "Current number is git number : "<< i <<endl;
-  }
+  }*/
+
+  cout<< "KOLOI YA SELAHLA YA NGE SENYENTSA BOPHELO"<<endl;
+
+
 
  return 0;
 }
